9d5764a075bf68ef8b16245266d3bac6aa55d2cc
kerwin chen <kerwincsc@foxmail.com>
2021-03-26 13:37:09 +0800

    version: 0.1 -> 0.2

--- conf.py
+++ conf.py
@@ -27,7 +27,7 @@ copyright = '2019, kerwin'
 author = 'kerwincsc@foxmail.com'
 
 # The short X.Y version
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags
 release = 'alpha'
 
